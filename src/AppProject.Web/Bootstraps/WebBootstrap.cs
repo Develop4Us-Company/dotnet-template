3b1a58a45ac847f7ea3fd450d29a35c66c0ad66c
Juarez Júnior <juarez.a.s.junior@gmail.com>
2025-09-11 11:53:23 -0300

    Changing web bootstrap.

---
+++ src/AppProject.Web/Bootstraps/WebBootstrap.cs
@@ -0,0 +1,58 @@
+using System;
+using AppProject.Web.Pages;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Refit;
+
+namespace AppProject.Web.Bootstraps;
+
+public static class WebBootstrap
+{
+    public static async Task ConfigureAndRunAsync(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        ConfigureAuthentication(builder);
+
+        ConfigureRefit(builder);
+
+        await builder.Build().RunAsync();
+    }
+
+    private static void ConfigureAuthentication(WebAssemblyHostBuilder builder)
+    {
+        // Configuração do Auth0
+        builder.Services.AddOidcAuthentication(options =>
+        {
+            builder.Configuration.Bind("Auth0", options.ProviderOptions);
+            options.ProviderOptions.ResponseType = "code";
+            options.ProviderOptions.DefaultScopes.Add("openid");
+            options.ProviderOptions.DefaultScopes.Add("profile");
+            options.ProviderOptions.DefaultScopes.Add("email");
+            options.ProviderOptions.DefaultScopes.Add("offline_access");
+
+            options.ProviderOptions.AdditionalProviderParameters.Add("audience", builder.Configuration["Auth0:Audience"]);
+        });
+    }
+
+    private static void ConfigureRefit(WebAssemblyHostBuilder builder)
+    {
+        // Configuração do Refit
+        builder.Services
+            .AddRefitClient<ISampleClient>()
+            .ConfigureHttpClient(c =>
+            {
+                c.BaseAddress = new Uri("https://localhost:7121");
+            })
+            .AddHttpMessageHandler(sp =>
+            {
+                var handler = sp.GetService<AuthorizationMessageHandler>()
+                    .ConfigureHandler(authorizedUrls: new[] { "https://localhost:7121" });
+
+                return handler;
+            });
+    }
+}
