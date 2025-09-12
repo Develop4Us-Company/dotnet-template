65091869f4d34c42977c1dbce854bc88def4908e
Juarez Júnior <juarez.a.s.junior@gmail.com>
2025-09-12 16:33:11 -0300

    Refit changes.

--- src/AppProject.Web/Bootstraps/WebBootstrap.cs
+++ src/AppProject.Web/Bootstraps/WebBootstrap.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Globalization;
+using System.Reflection;
 using AppProject.Web.Constants;
 using AppProject.Web.Options;
 using AppProject.Web.Pages;
@@ -71,20 +72,29 @@ public static class WebBootstrap
 
     private static void ConfigureRefit(WebAssemblyHostBuilder builder)
     {
-        // Configuração do Refit
-        builder.Services
-            .AddRefitClient<ISampleClient>()
-            .ConfigureHttpClient(c =>
-            {
-                c.BaseAddress = new Uri("https://localhost:7121");
-            })
-            .AddHttpMessageHandler(sp =>
-            {
-                var handler = sp.GetService<AuthorizationMessageHandler>()
-                    .ConfigureHandler(authorizedUrls: new[] { "https://localhost:7121" });
+        var refitInterfaces = GetApiClientAssemblies()
+            .SelectMany(x => x.GetTypes())
+            .Where(x => x.IsInterface);
 
-                return handler;
-            });
+        var apiOptions = new ApiOptions();
+        builder.Configuration.GetSection("Api").Bind(apiOptions);
+
+        if (string.IsNullOrEmpty(apiOptions.BaseUrl))
+        {
+            throw new InvalidOperationException("Api:BaseUrl configuration is missing.");
+        }
+
+        foreach (var refitInterface in refitInterfaces)
+        {
+            builder.Services
+                .AddRefitClient(refitInterface)
+                .ConfigureHttpClient(c =>
+                {
+                    c.BaseAddress = new Uri(apiOptions.BaseUrl);
+                })
+                .AddHttpMessageHandler(sp => sp.GetService<AuthorizationMessageHandler>()
+                    .ConfigureHandler(authorizedUrls: new[] { apiOptions.BaseUrl }));
+        }
     }
 
     private static async Task SetLanguageAsync(WebAssemblyHost host)
@@ -98,5 +108,16 @@ public static class WebBootstrap
 
         CultureInfo.DefaultThreadCurrentCulture = culture;
         CultureInfo.DefaultThreadCurrentUICulture = culture;
+    }
+
+    private static IEnumerable<Assembly> GetApiClientAssemblies() =>
+        [
+            Assembly.Load("AppProject.Web.ApiClient"),
+            Assembly.Load("AppProject.Web.ApiClient.General"),
+        ];
+
+    private class ApiOptions
+    {
+        public string BaseUrl { get; set; }
     }
 }
